ff19abf5f2d8c5f632ebcdb01c1c4cb0678cbd96
3li-3bdullah <PROGRMMER7@gmail.com>
2022-04-09 16:25:14 +0200

    I've modified the name

--- breaking_bad/lib/app_router.dart
+++ breaking_bad/lib/app_router.dart
@@ -1,13 +1,16 @@
-import 'package:breaking_bad/view/screens/charactrers_screen.dart';
+import 'package:breaking_bad/constants/strings.dart';
+import 'package:breaking_bad/view/screens/home.dart';
+import 'package:breaking_bad/view/screens/details_screen.dart';
 import 'package:flutter/material.dart';
 
-class AppRouter{
-  Route? generateRouters(RouteSettings settings){
+class AppRouter {
+  Route? generateRouters(RouteSettings settings) {
     switch (settings.name) {
-      case '/':
-        return MaterialPageRoute(builder: (_) => const CharactersScreen());
+      case home:
+        return MaterialPageRoute(builder: (_) => const Home());
+      case detailsScreen:
+        return MaterialPageRoute(builder: (_) => const DetailsScreen());
     }
     return null;
-    
   }
 }
