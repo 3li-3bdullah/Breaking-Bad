2ea08fb7d456da28a725674801150d86cd5c890b
3li-3bdullah <PROGRMMER7@gmail.com>
2022-04-09 16:28:51 +0200

    I've modified the detailsScreen name

--- breaking_bad/lib/app_router.dart
+++ breaking_bad/lib/app_router.dart
@@ -1,6 +1,6 @@
 import 'package:breaking_bad/constants/strings.dart';
 import 'package:breaking_bad/view/screens/home.dart';
-import 'package:breaking_bad/view/screens/details_screen.dart';
+import 'package:breaking_bad/view/screens/details.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
@@ -8,8 +8,8 @@ class AppRouter {
     switch (settings.name) {
       case home:
         return MaterialPageRoute(builder: (_) => const Home());
-      case detailsScreen:
-        return MaterialPageRoute(builder: (_) => const DetailsScreen());
+      case details:
+        return MaterialPageRoute(builder: (_) => const Details());
     }
     return null;
   }
