cb392da8568ebba15607458e4bac03daa33a71be
3li-3bdullah <PROGRMMER7@gmail.com>
2022-09-07 09:58:03 +0200

    I've created characters model

---
+++ breaking_bad/lib/data/model/characters.dart
@@ -0,0 +1,25 @@
+class Characters {
+  int? charId;
+  String? name;
+  String? nickName;
+  String? image;
+  List<dynamic>? jobs;
+  String? statusDeadOrAlive;
+  List<dynamic>? appearanceOfSeasons;
+  List<dynamic>? betteryCallSaulAppearnce;
+  String? actorName;
+  String? categoryFortwoSeries;
+
+  Characters.fromJson(Map<String, dynamic> json) {
+    charId = json['char_id'];
+    name = json['name'];
+    nickName = json['nickname'];
+    image = json['img'];
+    jobs = json['occupation'];
+    statusDeadOrAlive = json['status'];
+    appearanceOfSeasons = json['appearance'];
+    betteryCallSaulAppearnce = json['better_call_saul_appearance'];
+    actorName = json['portrayed'];
+    categoryFortwoSeries = json['category'];
+  }
+}
