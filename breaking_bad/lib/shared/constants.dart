edb4af83e58f4cd90cf73219358dfdbda7b11a18
3li-3bdullah <PROGRMMER7@gmail.com>
2022-09-07 09:57:33 +0200

    Added baseUrl for breaking Bad & two routes name and class MyColor that has three colors [myYello , myGrey , myWhite ]

---
+++ breaking_bad/lib/shared/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const baseUrl = 'https://www.breakingbadapi.com/api/';
+
+// Routes Names
+const String charactersScreen = '/';
+const String detailsScreen = '/details_screen';
+
+// Colors
+class MyColors {
+  static const myYellow = Color(0xffffc107);
+  static const myGrey = Color(0xff344a40);
+  static const myWhite = Color(0xffe1e8eb);
+}
