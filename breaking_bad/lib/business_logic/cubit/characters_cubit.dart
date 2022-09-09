4587b46ff183803a063f74bc55ef6d9d7ee221d6
3li-3bdullah <PROGRMMER7@gmail.com>
2022-09-09 15:25:26 +0200

    Created new function that will receive data from Class CharactersRepo

--- breaking_bad/lib/business_logic/cubit/characters_cubit.dart
+++ breaking_bad/lib/business_logic/cubit/characters_cubit.dart
@@ -1,8 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:breaking_bad/data/model/characters.dart';
+import 'package:breaking_bad/data/repository/characters_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'characters_state.dart';
 
 class CharactersCubit extends Cubit<CharactersState> {
-  CharactersCubit() : super(CharactersInitial());
+  CharactersCubit(this.charactersRepo) : super(CharactersInitial());
+
+  final CharactersRepo charactersRepo;
+  late List<Characters> characters;
+
+  List<Characters> getCharactersData(){
+    charactersRepo.getAllCharactersData().then((characters) {
+      emit(CharactersLoaded(characters));
+      this.characters = characters;
+    });
+    return characters;
+  }
 }
